c4acaed9397d263d1e35adfb1bcadf99b2420b8c
20sleibowitz <44878090+20sleibowitz@users.noreply.github.com>
2018-11-29 13:51:38 -0500

    Update Round.java

--- Round.java
+++ Round.java
@@ -3,6 +3,6 @@ public class Round {
 	
 	int numCandidates;
 	ArrayList<ArrayList<String>> master = new ArrayList<ArrayList<String>>();
-	ArrayList<ArrayList<String>>[] = new ArrayList<ArrayList<String>>[numCandidates];
+	ArrayList<ArrayList<String>>[] byCand = new ArrayList<ArrayList<String>>[numCandidates];
 	
 }
