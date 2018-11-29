5a805b0d6a04d005b60f71879f0c34f0c3bd096a
20sleibowitz <44878090+20sleibowitz@users.noreply.github.com>
2018-11-29 14:11:15 -0500

    Add files via upload

---
+++ Round.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class Round {
+
+	int numCandidates; 
+	ArrayList<ArrayList<String>> master = new ArrayList<ArrayList<String>>();
+	ArrayList<ArrayList<ArrayList<String>>> byCand = new ArrayList<ArrayList<ArrayList<String>>>(); 
+}
