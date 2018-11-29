3a7f82a71f1565f0de738f053ed15edb5e4aacb7
20sleibowitz <44878090+20sleibowitz@users.noreply.github.com>
2018-11-29 14:11:01 -0500

    Delete Round.java

--- Round.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-public class Round {
-	
-	int numCandidates;
-	ArrayList<ArrayList<String>> master = new ArrayList<ArrayList<String>>();
-	ArrayList<ArrayList<String>>[] byCand = new ArrayList<ArrayList<String>>[numCandidates];
-	
-}
