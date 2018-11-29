1794a4728fd27d854a00f8c38bd95722f5d811e3
20sleibowitz <44878090+20sleibowitz@users.noreply.github.com>
2018-11-29 14:11:45 -0500

    Update Round.java

--- Round.java
+++ Round.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 public class Round {
 
 	int numCandidates; 
+	//Not sure if numcands should be in here 
 	ArrayList<ArrayList<String>> master = new ArrayList<ArrayList<String>>();
 	ArrayList<ArrayList<ArrayList<String>>> byCand = new ArrayList<ArrayList<ArrayList<String>>>(); 
 }
