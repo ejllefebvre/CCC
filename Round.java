088fa52af1547af9cf69bbd337ef8e6fb514a140
Jgross02 <31026811+Jgross02@users.noreply.github.com>
2018-11-30 13:59:43 -0500

    Add files via upload

--- Round.java
+++ Round.java
@@ -1,8 +1,142 @@
 import java.util.ArrayList;
-public class Round {
 
-	int numCandidates; 
-	//Not sure if numcands should be in here 
-	ArrayList<ArrayList<String>> master = new ArrayList<ArrayList<String>>();
-	ArrayList<ArrayList<ArrayList<String>>> byCand = new ArrayList<ArrayList<ArrayList<String>>>(); 
+public class Round {
+	
+	int numCandidates;
+	int majority;
+	Boolean winner;
+	Candidate winningCandidate;
+	
+	ArrayList<Vote> master;
+	ArrayList<Vote> masterCopy;
+	ArrayList<Vote> nextRound;
+	
+	ArrayList<Candidate> byCandidate;
+	ArrayList<Candidate> nextRoundCandidates;
+	
+	public Round(ArrayList<Candidate> candidates, ArrayList<Vote> input) {
+		master = input;
+		masterCopy = createCopy(master);
+		majority = master.size()/2;
+		
+		byCandidate = candidates;
+		nextRound = new ArrayList<Vote>();
+		nextRoundCandidates = new ArrayList<Candidate>();
+		
+		sortMaster();
+		printResults();
+		if(winner) {
+			System.out.println();
+			System.out.println(winningCandidate.getName() + " has won");
+		} else {
+			redistribute();
+			System.out.println(nextRound);
+			System.out.println();
+			System.out.println();
+			new Round(this.getNextRoundCandidates(), this.getNextRoundList());
+		}
+	}
+	
+	public ArrayList<Vote> createCopy(ArrayList<Vote> masterInput) {
+		ArrayList<Vote> copy = new ArrayList<Vote>();
+		for(Vote vote: masterInput) {
+			copy.add((Vote) vote.clone());
+		}
+		
+		return copy;
+	}
+
+	public void checkForWinner() {
+		winner = false;
+		
+		for(Candidate candidate: byCandidate) {
+			if(candidate.getVoteScore() > majority) {
+				winner = true;
+				winningCandidate = candidate;
+			}
+		}
+	}
+	
+	public void sortMaster() {
+		for(Vote vote: masterCopy) {
+			sortVote(vote);
+		}
+	}
+	
+	public void sortVote(Vote vote) {
+		String validVote = vote.get(0);
+		
+		for(Candidate candidate: byCandidate) {
+			if(validVote.equalsIgnoreCase(candidate.getName())) {
+				candidate.addVote(vote);
+			}
+		}
+	}
+	
+	public void printResults() {
+		for(Candidate candidate: byCandidate) {
+			System.out.println(candidate.getName() + ": " + candidate.getVoteScore());
+			for(Vote vote: candidate.getVotes()) {
+				System.out.println(vote);
+			}
+			
+			checkForWinner();
+		}
+	}
+	
+	public void redistribute() {
+		int index = findMinimum(byCandidate);
+		
+		for(Vote vote: byCandidate.get(index).getVotes()) {
+			vote.remove(0);
+		}
+		
+		createNextRoundList();
+		
+		for(Candidate candidate: byCandidate) {
+			candidate.getVotes().clear();
+		}
+	}
+	
+	public void createNextRoundList() {
+		for(Candidate candidate: byCandidate) {
+			for(Vote vote: candidate.getVotes()) {
+				vote.nextValidVote(nextRoundCandidates);
+				nextRound.add(vote);
+			}
+		}
+	}
+	
+	public int findMinimum(ArrayList<Candidate> candidates) {
+		int minimumVotes = candidates.get(0).getVoteScore();
+		int minimumIndex = 0;
+		
+		for(int i=0; i<candidates.size(); i++) {
+			if(candidates.get(i).getVoteScore() < minimumVotes && candidates.get(i).getVoteScore() > 0) {
+				minimumVotes = candidates.get(i).getVoteScore();
+				minimumIndex = i;
+			} 
+		}
+		
+		createNextRoundCandidates(candidates, minimumIndex);
+		
+		return minimumIndex;
+	}
+	
+	public void createNextRoundCandidates(ArrayList<Candidate> candidates, int minimumIndex) {
+		for(int i=0; i<candidates.size(); i++) {
+			if(i != minimumIndex && !(candidates.get(i).getVoteScore() == 0)) {
+				nextRoundCandidates.add(candidates.get(i));
+			}
+			
+		}
+	}
+
+	public ArrayList<Vote> getNextRoundList() {
+		return nextRound;
+	}
+	
+	public ArrayList<Candidate> getNextRoundCandidates() {
+		return nextRoundCandidates;
+	}
 }
